675917b6744b791d88ca37fff012ffb3e08f00ab
aditya <addyranjan4242@gmail.com>
2024-08-06 22:57:34 +0530

    Finding Grade marks of given number

--- src/GradeSystem.java
+++ src/GradeSystem.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class GradeSystem {
+public class GradeSystem {
+    public static void main(String args[]) {
+        String res = getGrade(100);
+        System.out.println("A Student got " + res);
+    }
+
+    private static String getGrade(int marks) {
+
+        if (marks > 90 && marks <= 100) {
+            return "Grade A+";
+        } else if (marks > 80 && marks <= 90) {
+            return "Grade A";
+        } else if (marks > 70 && marks <= 80) {
+            return "Grade B";
+        } else if (marks > 60 && marks <= 70) {
+            return "Grade c";
+        } else {
+            return "Fail";
+        }
+
+    }
+
+
 }
